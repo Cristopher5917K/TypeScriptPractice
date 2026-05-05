8fdf953a09712bff6e17aa239ce5347e0a7cf1f3
Cristopher5917K <garcacristopher3@gmail.com>
2026-05-05 11:05:51 -0500

    Delete ElectroStock directory

--- ElectroStock/src/udla/edu/cgarcia/mrevelo/cespinoza/Accesorios/Tipo.java
+++
@@ -1,8 +0,0 @@
-package udla.edu.cgarcia.mrevelo.cespinoza.Accesorios;
-
-public enum Tipo {
-    FUNDA,
-    CABLE,
-    ADAPTADOR,
-    PAD,
-}
--- ElectroStock/src/udla/edu/cgarcia/mrevelo/cespinoza/periferico/TipoPeriferico.java
+++
@@ -1,7 +0,0 @@
-package udla.edu.cgarcia.mrevelo.cespinoza.periferico;
-
-public enum TipoPeriferico {
-    ENTRADA,
-    SALIDA,
-    ALMACENAMIENT,
-}
